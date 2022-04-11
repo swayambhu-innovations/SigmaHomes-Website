f47060cb72df8c90a06fd182310dbf19f8689569
Kumar Saptam <saptampro2003@gmail.com>
2022-04-11 20:52:50 +0530

    Added firebase configurations.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sigmahomes-3dae4',
+    appId: '1:162569177911:web:96fe2c52e45eec1f2da8b2',
+    storageBucket: 'sigmahomes-3dae4.appspot.com',
+    apiKey: 'AIzaSyDRkuMVSVTiYXr_RuhfMQPYlVFG3Qg6eMg',
+    authDomain: 'sigmahomes-3dae4.firebaseapp.com',
+    messagingSenderId: '162569177911',
+    measurementId: 'G-LPL6SL85Y2',
+  },
   production: false
 };
 
