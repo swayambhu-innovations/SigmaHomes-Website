c268235fb7c89587a1f6410250ec308d929f6770
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-05-29 03:51:02 +0530

    Solved the click issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     loadChildren: () =>
-      import('./customer/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'login',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./customer/login/login.module').then((m) => m.LoginModule),
+      import('./customer/customer.module').then((m) => m.CustomerModule),
   },
   {
     path: 'admin',
@@ -25,36 +13,9 @@ const routes: Routes = [
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
   {
-    path: 'signup',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./customer/signup/signup.module').then((m) => m.SignupModule),
-  },
-  {
-    path: 'terms-and-conditions',
-    loadChildren: () =>
-      import(
-        './customer/terms-and-conditions/terms-and-conditions.module'
-      ).then((m) => m.TermsAndConditionsModule),
-  },
-  {
-    path: 'returns-and-refunds',
-    loadChildren: () =>
-      import(
-        './customer/return-refund-policy/return-refund-policy.module'
-      ).then((m) => m.ReturnRefundPolicyModule),
-  },
-  {
-    path: 'privacy',
-    loadChildren: () =>
-      import('./customer/privacy-policy/privacy-policy.module').then(
-        (m) => m.PrivacyPolicyModule
-      ),
-  },
-  {
     path: '**',
     loadChildren: () =>
-      import('./customer/not-found404/not-found404.module').then(
+      import('./not-found404/not-found404.module').then(
         (m) => m.NotFound404Module
       ),
   },
